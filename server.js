ef5f043356031a7b069bd7027c4a276318533ae4
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-01-03 10:31:18 -0300

    Ajustado o server.js  e o package para deploy

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const path = require('path');
-const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
-
+app.use(express.static("App")); 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+res.sendFile(`${__dirname}/app/index.html`);
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT);
