6882798758b41f82f26d71a0ed1e9d0b6551ab24
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-01-03 10:51:44 -0300

    Mudado o diretorio

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, 'app/index.html'))
 })
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/app'))
 
 app.listen(process.env.PORT || 3000)
