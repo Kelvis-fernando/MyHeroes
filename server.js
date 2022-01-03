0b14b3984326e262040e226f1a3048e9494bfeb0
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-01-03 10:17:17 -0300

    Alterado o server.js para deploy

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static("App"));  
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+
 app.get('/*', (req, res) => {
-  res.sendFile(`${__dirname}/app/index.html`);
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 8080);
